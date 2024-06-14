b07ac17b67bb9a7b2cb31c8f56b8ba806c9b088e
Csizmadia László <csimadialacko@gmail.com>
2024-06-14 11:23:18 +0300

    NEXTAUTH_URL removed

--- app/utils/db.ts
+++ app/utils/db.ts
@@ -3,6 +3,7 @@ import { PrismaClient } from "@prisma/client";
 const prismaClientSingleton = () => {
   return new PrismaClient();
 };
+
 declare const globalThis: {
   prismaGlobal: ReturnType<typeof prismaClientSingleton>;
 } & typeof global;
