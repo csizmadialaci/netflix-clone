0f68a10f90dfa50e6a179f64cd37dc6be2883092
Csizmadia László <csimadialacko@gmail.com>
2024-06-17 09:20:15 +0300

    Resend env items removed.

--- app/api/send/route.ts
+++ app/api/send/route.ts
@@ -9,7 +9,7 @@ export async function POST() {
     const { data, error } = await resend.emails.send({
       from: "Acme <onboarding@resend.dev>",
       to: ["delivered@resend.dev"],
-      subject: "Hello world, ",
+      subject: "Hello world",
       react: EmailTemplate({ firstName: "John" }) as React.ReactElement,
     });
 
